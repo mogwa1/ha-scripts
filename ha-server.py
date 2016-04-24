215af5afbeeeee7885786fa6e66629999e76b2f4
Bart De Vries <devriesb@gmail.com>
2016-04-24 22:50:03 +0200

    Adapt scripts to work over network.

--- ha-server.py
+++ ha-server.py
@@ -21,7 +21,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "0.0.0.0", 9999
 
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
